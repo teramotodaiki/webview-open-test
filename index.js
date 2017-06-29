d01ce1a7914d90c78584030743b68169766eb35a
teramotodaiki <i101323@gmail.com>
2017-06-29 19:10:48 +0900

    4

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 try {
   if ('localStorage' in window) {
-    document.write('localStorage exists');
+    document.write('localStorage ');
     console.info('setItem' in window.localStorage);
   } else {
     console.error('localStorage is not found');
   }
+  if ('sessionStorage' in window) {
+    document.write('sessionStorage');
+  }
+  if ('indexedDB' in window) {
+    document.write('indexedDB ');
+  }
+
   localStorage.setItem('hogehoge', 'fugafuga');
 } catch (e) {
   console.log('Error is occured');
